226b41d4e36dec5422f9a4db76f07884f79664ad
Eduardo Correa Araujo <eduardocorreaaraujo8@gmail.com>
2021-12-28 16:41:41 -0300

    Add ICU forecast

--- get_data.py
+++ get_data.py
@@ -14,7 +14,8 @@ from sqlalchemy import create_engine
 import matplotlib.pyplot as plt 
 #import streamlit as st 
 
-engine = create_engine("postgresql://epigraph:epigraph@localhost:5432/epigraphhub")
+engine_public = create_engine("postgresql://epigraph:epigraph@localhost:5432/epigraphhub")
+engine_private = create_engine("postgresql://epigraph:epigraph@localhost:5432/privatehub")
 
 def build_lagged_features(dt, maxlag=2, dropna=True):
     '''
@@ -92,7 +93,7 @@ def compute_clusters(curve, t, plot = False):
     -> all_regions: is the array with all the regions
     '''
     
-    df = pd.read_sql_table(f'foph_{curve}', engine, schema = 'switzerland', columns = ['datum','geoRegion',  'entries'])
+    df = pd.read_sql_table(f'foph_{curve}', engine_public, schema = 'switzerland', columns = ['datum','geoRegion',  'entries'])
     df.index = pd.to_datetime(df.datum)
     
     inc_canton = df.pivot(columns='geoRegion', values='entries')
@@ -111,15 +112,17 @@ def compute_clusters(curve, t, plot = False):
     # Plotting the dendrogram
     linkage = hcluster.linkage(cm, method='complete')
    
+   
+    if plot: 
+        fig, ax = plt.subplots(1,1, figsize=(15,10), dpi = 300)
+        hcluster.dendrogram(linkage, labels=inc_canton.columns, color_threshold=0.3, ax=ax)
+        ax.set_title('Result of the hierarchical clustering of the series', fontdict= {'fontsize': 20})
+        plt.xticks(fontsize = 16)
+        plt.yticks(fontsize = 16)
+        
+    else: 
+        fig = None
     
-    fig, ax = plt.subplots(1,1, figsize=(15,10), dpi = 300)
-    hcluster.dendrogram(linkage, labels=inc_canton.columns, color_threshold=0.3, ax=ax)
-    ax.set_title('Result of the hierarchical clustering of the series', fontdict= {'fontsize': 20})
-    plt.xticks(fontsize = 16)
-    plt.yticks(fontsize = 16)
-    
-    if plot:
-        plt.show()
 
     # computing the cluster 
     
@@ -131,6 +134,31 @@ def compute_clusters(curve, t, plot = False):
     all_regions = df.geoRegion.unique() 
     
     return clusters, all_regions, fig
+
+def get_updated_data(smooth):
+    
+    '''
+    Function to get the updated data for Geneva
+    
+    param smooth: Boolean. If True, a rolling average is applied
+    
+    return: dataframe. 
+    '''
+    
+    df = pd.read_sql_table('hug_hosp_data', engine_private, schema = 'switzerland', columns = ['Date_Entry', 'Patient_id'])
+   
+    df.index = pd.to_datetime(df.Date_Entry)
+    df_hosp = df.resample('D').count()
+    df_hosp = df_hosp[['Patient_id']]
+    
+    if smooth == True:
+        df_hosp  = df_hosp[['Patient_id']].rolling(window = 7).mean()
+        df_hosp = df_hosp.dropna()
+        
+    df_hosp = df_hosp.sort_index()
+    df_hosp.rename(columns= {'Patient_id':'hosp_GE'}, inplace = True)
+    return df_hosp['2021-09-01':]
+    
 
 #@st.cache  
 def get_cluster_data(curve, georegion):
@@ -195,14 +223,14 @@ def get_combined_data( data_types, georegion,vaccine = True, smooth = True):
             df = df.merge(get_cluster_data(data_types[i], georegion), left_index =True, right_index = True)
     
     if vaccine == True:
-        ## adicionando os dados de vacinação para a suiça disponibilizados pelo 
-        ## Our World in Data
+        ## add the vaccine data for Switzerland made available by Our world in Data 
         vac = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
         
-        # selecionando apenas os dados da suiça 
+        # selecting the switzerland data 
         vac = vac.loc[vac.iso_code == 'CHE']
         vac.index = pd.to_datetime(vac.date)
         
+        # selecting only the column with vaccinations per hundred 
         vac = vac[['total_vaccinations_per_hundred']]
         
         vac = vac.fillna(0)
@@ -228,15 +256,27 @@ def get_combined_data( data_types, georegion,vaccine = True, smooth = True):
 
 #@st.cache 
 def get_canton_data(curve, canton, ini_date = None):
+    '''
+    This function provide a dataframe for the curve selected in the param curve and
+    the canton selected in the param canton
     
+    param curve: strin. One of the following options are accepted: ['cases', 'casesVaccPersons', 'covidCertificates', 'death',
+                                                             'deathVaccPersons', 'hosp', 'hospCapacity', 'hospVaccPersons',
+                                                             'intCases', 're', 'test', 'testPcrAntigen', 'virusVariantsWgs']
+    param canton: array with all the cantons of interest.
+
+    return dataframe
+    '''
+    
+    # dictionary with the columns that will be used for each curve. 
     dict_cols = {'cases':['geoRegion','datum','entries'], 'test': ['geoRegion','datum','entries', 'entries_pos'],
                 'hosp':['geoRegion','datum','entries'], 'hospcapacity':['geoRegion','date','ICU_Covid19Patients'], 're': ['geoRegion', 'date','median_R_mean']
                 }
     
-    df = pd.read_sql_table(f'foph_{curve}', engine, schema = 'switzerland', columns = dict_cols[curve])
+    # getting the data from the databank
+    df = pd.read_sql_table(f'foph_{curve}', engine_public, schema = 'switzerland', columns = dict_cols[curve])
     
-    
-    
+    # filtering by cantons
     df = df.loc[ (df.geoRegion.isin(canton))]
     
     if (curve == 're') | (curve =='hospcapacity'):
